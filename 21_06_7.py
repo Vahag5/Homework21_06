c97abc3dd13cc0eaf0c5f1b9987e951510715523
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-27 15:59:26 +0400

    Update 21_06_7.py

--- 21_06_7.py
+++ 21_06_7.py
@@ -1,17 +1,27 @@
-num1 = int(input("Greq arajin tiv@: "))
-num2 = int(input("Greq arajin tiv@: "))
-gorc = input("please select +, -, / or *: ")
-if gorc == '+' :
-    sum = num1 + num2
-elif gorc == '-':
-    sum = num1 - num2
-elif gorc == '*': 
-    sum = num1 * num2
-elif gorc == '/' and num2 != 0:
-    sum = num1 / num2 
-else :
+while True:
+    num1 = int(input("Greq arajin tiv@: "))
+    num2 = int(input("Greq ekrord tiv@: "))
+    gorc = input("please select +, -, /, * or Q(For quit): ")
+    if gorc == '+' :
+        sum = num1 + num2
+    elif gorc == '-':
+        sum = num1 - num2
+    elif gorc == '*': 
+        sum = num1 * num2
+    elif gorc == '/' and num2 != 0:
+        sum = num1 / num2
+    elif gorc == 'Q' :
+        break
+    else :
         sum = "Haytararum chi karox linel 0"
-print(sum)
+    print(sum)
+    
+
+
+    
+
+
+
 
 
 
